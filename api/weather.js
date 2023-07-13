f02714f30f372081d20a549c2a9823dc519e331c
unknown <naseer190100@gmail.com>
2023-07-13 23:15:36 +0500

    changes

--- api/weather.js
+++ api/weather.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 import { apiKey } from "../constants";
 
+/**
+ * The code defines two functions that generate API endpoints for weather forecasts and location
+ * searches using the WeatherAPI.
+ */
 const forecastEndpoint = params=> `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}`;
 const locationsEndpoint = params=> `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
+/**
+ * The code exports two functions, `fetchWeatherForecast` and `fetchLocations`, which make API calls to
+ * retrieve weather forecasts and locations respectively.
+ * @param endpoint - The `endpoint` parameter is the URL of the API endpoint that you want to make a
+ * request to. It could be the URL of a weather forecast API or a locations API, depending on the
+ * function you are calling.
+ * @returns The `fetchWeatherForecast` function and `fetchLocations` function are returning the result
+ * of the `apiCall` function.
+ */
 const apiCall = async (endpoint)=>{
     const options = {
         method: 'GET',
--- utils/asyncStorage.js
+++ utils/asyncStorage.js
@@ -1,5 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+/**
+ * The above code defines two functions, `storeData` and `getData`, that can be used to store and
+ * retrieve data from AsyncStorage in JavaScript.
+ * @param key - The key parameter is a string that represents the unique identifier for the data you
+ * want to store or retrieve from AsyncStorage. It is used to associate a value with a specific key in
+ * the storage.
+ * @param value - The value parameter is the data that you want to store in AsyncStorage. It can be of
+ * any type, such as a string, number, object, or array.
+ */
 export const storeData = async (key, value) => {
   try {
     await AsyncStorage.setItem(key, value);
